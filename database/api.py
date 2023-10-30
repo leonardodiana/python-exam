de269a9e8997611704bcac613a769a91cdecd125
Leonardo Diana <ICTS22-24.473@studenti.local>
2023-10-30 12:08:08 +0100

    .

--- database/api.py
+++ database/api.py
@@ -1,20 +1,15 @@
-# from fastapi import FastAPI
-# import sqlite3
-# from database.db import *
-# import numpy as np
+from fastapi import FastAPI
+import sqlite3
+from database.db import *
+import numpy as np
 
 
 
 
 
-# app = FastAPI()
+app = FastAPI()
 
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-
-
-# # @app.get("/wine")
-# # def read_all_wine_endpoint():
-# #     return read_all_wines()
+@app.get("/wine")
+def read_all_wine_endpoint():
+    return read_all_wines()
